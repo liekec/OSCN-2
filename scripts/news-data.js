b171ab663c9762498213fd5ba13fdb31e0c13492
liekec <lieke.corbeek@ru.nl>
2026-09-09 10:54:09 +0200

    Update news-data.js

--- scripts/news-data.js
+++ scripts/news-data.js
@@ -4,6 +4,7 @@ const newsArticles = [
     slug: "turning-the-tide",
     date: "April 16, 2026",
     title: "Turning the Tide: Stories of Open Science",
+    image: "../images/news/turning-the-tide.jpg",
     content: `
       <p>During the Open Science Retreat in Schoorl, a project was developed titled "Turning the Tide: Stories of Open Science." In this short film, featuring seven people with different roles in academia, researchers share their personal turning points. These are the moments that led them toward open science. They also reflect on what openness in research means in their own work.</p>
       <p>This film aims to empower others to also start their journey, which might be feeling like going against the tide. But maybe you are riding a new wave, together with more and more of your peers?</p>
